28c5f2172ef7944472e1e273bd4fa9a4d0405f77
AlanGabbianelli <alan.gabbianelli@gmail.com>
2016-05-18 16:52:34 +0100

    Refactor checkout spec

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,21 +1,21 @@
 require 'checkout'
 
 describe Checkout do
-  subject(:co) { described_class.new }
+  subject(:checkout) { described_class.new }
   let(:lavander_heart) { double(:item) }
   let(:personalised_cufflinks) { double(:item) }
 
   context 'without items' do
     it 'starts with an empty basket' do
-      expect(co.basket).to be_empty
+      expect(checkout.basket).to be_empty
     end
 
     it 'starts with an empty order' do
-      expect(co.order).to be_empty
+      expect(checkout.order).to be_empty
     end
 
     it 'starts with a subtotal of 0.00' do
-      expect(co.subtotal).to eq(0.00)
+      expect(checkout.subtotal).to eq(0.00)
     end
   end
 
@@ -25,20 +25,20 @@ describe Checkout do
       allow(personalised_cufflinks).to receive(:price).and_return(45.00)
       allow(lavander_heart).to receive(:code).and_return(001)
       allow(personalised_cufflinks).to receive(:code).and_return(002)
-      co.scan(lavander_heart)
-      co.scan(personalised_cufflinks)
+      checkout.scan(lavander_heart)
+      checkout.scan(personalised_cufflinks)
     end
 
     it 'can add items to basket' do
-      expect(co.basket).to eq([lavander_heart, personalised_cufflinks])
+      expect(checkout.basket).to eq([lavander_heart, personalised_cufflinks])
     end
 
     it 'can update order with items\' codes and quantity' do
-      expect(co.order).to eq(001 => 1, 002 => 1)
+      expect(checkout.order).to eq(001 => 1, 002 => 1)
     end
 
     it 'can calculate the subtotal of the basket' do
-      expect(co.subtotal).to eq(54.25)
+      expect(checkout.subtotal).to eq(54.25)
     end
   end
 end
